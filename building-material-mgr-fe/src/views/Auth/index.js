73d1a422150cb57e80a1182ea8aa079fbbf7b1c9
15897736196 <2585644245@qq.com>
2022-03-08 20:50:40 +0800

    完成前端登入注册等相关页面

---
+++ building-material-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,13 @@
+import{defineAsyncComponent, defineComponent} from 'vue';
+import{UserOutlined,LockOutlined,SmileOutlined }from '@ant-design/icons-vue';
+export default defineComponent({
+
+   components:{
+      UserOutlined,
+      LockOutlined,
+      SmileOutlined,
+   },
+   setup(){
+       
+   }
+});
---
+++ building-material-mgr-fe/src/views/Auth/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="auth">
+    <div class="bg"></div>
+    <div class="title-info">
+      <img src="" alt="" />
+      <div class="title">建材销售管理系统</div>
+    </div>
+
+    <div class="form">
+      <a-tabs>
+        <a-tab-pane key="1" tab="登入">
+          <div class="item">
+            <a-input placeholder="账户">
+              <template v-slot:prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a-input placeholder="密码">
+              <template v-slot:prefix>
+                <LockOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a href="">忘记密码</a>
+          </div>
+
+          <div class="item">
+            <a-button type="primary"> 登入 </a-button>
+          </div>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="注册">
+          <div class="item">
+            <a-input placeholder="账户">
+              <template v-slot:prefix>
+                <UserOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a-input placeholder="密码">
+              <template v-slot:prefix>
+                <LockOutlined />
+              </template>
+            </a-input>
+          </div>
+
+           <div class="item">
+            <a-input placeholder="邀请码">
+              <template v-slot:prefix>
+                <SmileOutlined />
+              </template>
+            </a-input>
+          </div>
+
+          <div class="item"></div>
+
+          <div class="item">
+            <a-button type="primary"> 注册 </a-button>
+          </div>
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+    <!-- <a-button type="primary">Primary Button</a-button> -->
+  </div>
+</template>
+
+<script src="./index.js"></script>
+//引入组件逻辑
+
+<style lang="scss" scpoed>
+@import "./index.scss";
+</style>
